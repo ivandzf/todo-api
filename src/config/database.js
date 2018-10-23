b83d912d980e8ec48f533458e2727faa5c4b681a
ivandzf <divanandazf@gmail.com>
2018-10-23 21:16:16 +0700

    travis environment

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
 	process.env.NODE_ENV == 'production' ? 'kqwjvoyv' : 'todos',
 	process.env.NODE_ENV == 'production' ? 'kqwjvoyv' : '',
-	process.env.NODE_ENV == 'production'
-		? 'yMsmCDMj4Lx8aD80psg215kgpdGpcRbp'
-		: '',
+	process.env.NODE_ENV == 'production' ? 'yMsmCDMj4Lx8aD80psg215kgpdGpcRbp' : '',
 	{
 		host: process.env.NODE_ENV == 'production' ? 'elmer.db.elephantsql.com' : 'localhost',
 		dialect: 'postgres',
