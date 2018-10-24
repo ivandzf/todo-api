c1db15fc36edd47c221ad934fdecc594e8709b3a
ivandzf <divanandazf@gmail.com>
2018-10-24 11:15:43 +0700

    fix class test

--- src/test/project-test.js
+++ src/test/project-test.js
@@ -7,15 +7,15 @@ chai.use(chaiHttp);
 
 const projectUrl = '/api/v1/project';
 const projectDumy = {
-	name: 'test project',
-	order: 1
+	name: 'test project'
 };
 
 describe('Save project', () => {
 	it('return must create one project', done => {
 		chai
 			.request(server)
-			.post(projectDumy)
+			.post(projectUrl)
+			.send(projectDumy)
 			.end((err, res) => {
 				res.should.have.status(201);
 				done();
@@ -31,6 +31,50 @@ describe('Get all project', () => {
 			.end((err, res) => {
 				res.should.have.status(200);
 				done();
+			});
+	});
+});
+
+describe('Update project', () => {
+	it('return must create update name project', done => {
+		chai
+			.request(server)
+			.get(projectUrl)
+			.end((err, res) => {
+				res.should.have.status(200);
+
+				chai
+					.request(server)
+					.put(
+						projectUrl + '/' + res.body.find(x => x.name == projectDumy.name).id
+					)
+					.send({ name: 'change name' })
+					.end((err, res) => {
+						res.should.have.status(204);
+						done();
+					});
+			});
+	});
+});
+
+describe('Delete project', () => {
+	it('return must delete one project', done => {
+		chai
+			.request(server)
+			.get(projectUrl)
+			.end((err, res) => {
+				res.should.have.status(200);
+
+				chai
+					.request(server)
+					.delete(
+						projectUrl + '/' + res.body.find(x => x.name == 'change name').id
+					)
+					.send()
+					.end((err, res) => {
+						res.should.have.status(204);
+						done();
+					});
 			});
 	});
 });
